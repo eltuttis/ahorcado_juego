4ca0a4effcc177b589a29dc6f82f78e2154248f3
Juan Daniel <jrodriguezgiraldo8@gmail.com>
2023-09-20 16:40:55 -0500

    final del ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -12,37 +12,37 @@ comidas = ['pizza', 'hamburguesa', 'tacos', 'sushi', 'pollo frito', 'ensalada', 
 paises = ['estados unidos', 'canada', 'mexico', 'brasil', 'argentina', 'francia', 'espana', 'italia', 'alemania', 'reino unido', 'japon', 'china', 'india', 'australia', 'nueva zelanda', 'sudafrica', 'egipto', 'nigeria', 'kenia', 'rusia']
 objetos = ['telefono', 'computadora', 'televisor', 'refrigerador', 'lavadora', 'secadora', 'horno microondas', 'tostadora', 'licuadora', 'aspiradora', 'cepillo de dientes', 'espejo', 'llaves', 'paraguas', 'libreta', 'boligrafo', 'lapiz', 'tijeras', 'cinta adhesiva', 'caja de herramientas']
 
-
-escogedor_palabra={
-    "animales": animales[random.randrange(len(animales))],
-    "comidas": comidas[random.randrange(len(comidas))],
-    "paises": paises[random.randrange(len(paises))],
-    "objetos": objetos[random.randrange(len(objetos))]
-}
+def escogedor_palabra(categoria):
+    if categoria ==1:
+        palabra_categoria= animales[random.randrange(len(animales))]
+    elif categoria ==2:
+        palabra_categoria= comidas[random.randrange(len(comidas))]
+    elif categoria ==3:
+        palabra_categoria= paises[random.randrange(len(paises))]
+    elif categoria ==4:
+        palabra_categoria= objetos[random.randrange(len(objetos))]
+    funcionamiento(palabra_categoria)
+           
+# escogedor_palabra={
+#     "animales": animales[random.randrange(len(animales))],
+#     "comidas": comidas[random.randrange(len(comidas))],
+#     "paises": paises[random.randrange(len(paises))],
+#     "objetos": objetos[random.randrange(len(objetos))]
+# }
 def salir():
      print("gracias por jugar")
 def inicio():
         print("Menú\n 1. Jugar\n 2. Como Jugar\n 3. Cerrar")
         r=int(input("Introduzca un número: "))
         if r==1:
-            funcionamiento()
+            dificultad_categoria()
         elif r==2:
             como_jugar()
         elif r==3:
              salir()
-        
-    
-def como_jugar():
-    print("El juego consta principalmente de unas categorias y unos niveles de Dificultad, irás colocando letras hasta completar la palabra sin que se te acaben las vidas, cuando se te acaben las vidas será un GameOver")
-    inicio()
-def vidas():
-    vida.pop()    
-    print(" ".join(vida))
-def funcionamiento():
-    count2=0
+def dificultad_categoria():
     print("Dificultades\n 1. Facil\n 2. Medio\n 3. Dificil\n 4. Master")
     p =int(input("ingrese dificultad: "))
-
     if p ==1:
         g=0
         for f in range(9):
@@ -60,17 +60,17 @@ def funcionamiento():
         for f in range(3):
             vida.append("♥")
     print("Categorias\n 1. animales\n 2. comidas\n 3. paises\n 4. objetos")
-    x =int(input("ingrese una categoria: "))
+    escogedor_palabra(int(input("ingrese una categoria: ")))
+    print(" ".join(vida))       
+    
+def como_jugar():
+    print("El juego consta principalmente de unas categorias y unos niveles de Dificultad, irás colocando letras hasta completar la palabra sin que se te acaben las vidas, cuando se te acaben las vidas será un GameOver")
+    inicio()
+def vidas():
+    vida.pop()    
     print(" ".join(vida))
-    if x==1:
-            t="animales"
-    elif x==2:
-            t="comidas"
-    elif x==3:
-            t="paises"
-    elif x==4:
-            t="objetos"
-    j = escogedor_palabra.get(t)
+def funcionamiento(j):
+    count2=0
     poner_quitar_letras = list(j)
     palabra= list(j) 
     while count2 != len(poner_quitar_letras):
@@ -92,9 +92,9 @@ def funcionamiento():
             letras_ingresadas.append(ingreso_de_letra)
         if count == 0:
             vidas()
-            g= g +1
-                
+            g = g+1
             grafica(g)    
+            return g
 
         print(" ".join(interfaz))
         print("letras ya ingresadas:"," ,".join(letras_ingresadas))
